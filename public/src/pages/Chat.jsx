df18dec51e4cbcb4c9e5332a5617d0274e109be6
Adilak007 <aryanadlakha01@gmail.com>
2024-08-01 21:06:11 +0530

    first-commit

---
+++ public/src/pages/Chat.jsx
@@ -0,0 +1,95 @@
+import React, { useEffect, useState, useRef } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import { allUsersRoute, host } from "../utils/APIRoutes";
+import ChatContainer from "../components/ChatContainer";
+import Contacts from "../components/Contacts";
+import Welcome from "../components/Welcome";
+import { io } from "socket.io-client";
+
+export default function Chat() {
+  const socket=useRef();
+  const navigate = useNavigate();
+  const [contacts, setContacts] = useState([]);
+  const [currentChat, setCurrentChat] = useState(undefined);
+  const [currentUser, setCurrentUser] = useState(undefined);
+
+  useEffect(() => {
+    async function checkUser() {
+      const user = localStorage.getItem("chat-app-user");
+      if (!user) {
+        navigate("/login");
+      } else {
+        setCurrentUser(JSON.parse(user));
+      }
+    }
+
+    checkUser();
+  }, [navigate])
+
+  useEffect(() => {
+    if (currentUser) {
+      socket.current = io(host);
+      socket.current.emit("add-user", currentUser._id);
+    }
+  }, [currentUser]);
+
+
+  useEffect(() => {
+    async function fetchContacts() {
+      if (currentUser) {
+        if (currentUser.isAvatarImageSet) {
+          try {
+            const response = await axios.get(`${allUsersRoute}/${currentUser._id}`);
+            setContacts(response.data);
+          } catch (error) {
+            console.error('Error fetching contacts:', error);
+          }
+        } else {
+          navigate("/setAvatar");
+        }
+      }
+    }
+    fetchContacts();
+  }, [currentUser, navigate, allUsersRoute]); // Add all dependencies
+
+  const handleChatChange = (chat) => {
+    setCurrentChat(chat);
+  };
+  return (
+    <>
+      <Container>
+        <div className="container">
+          <Contacts contacts={contacts} changeChat={handleChatChange} />
+          {currentChat === undefined ? (
+            <Welcome />
+          ) : (
+            <ChatContainer currentChat={currentChat} currentUser={currentUser} socket={socket}/>
+          )}
+        </div>
+      </Container>
+    </>
+  );
+}
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1rem;
+  align-items: center;
+  background-color: #131324;
+  .container {
+    height: 100vh;
+    width: 100vw;
+    background-color: #00000076;
+    display: grid;
+    grid-template-columns: 25% 75%;
+    @media screen and (min-width: 720px) and (max-width: 1080px) {
+      grid-template-columns: 35% 65%;
+    }
+  }
+`;
